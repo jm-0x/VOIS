28374e98490abba358992c451a06261509820000
Joonitoi <jon.milles@protonmail.com>
2020-12-01 00:54:32 +0100

    added test driver

--- Client/src/main.cpp
+++ Client/src/main.cpp
@@ -1,7 +1,25 @@
 #include <iostream>
-#include <functional>
-
+#include <asio.hpp>
 int main()
 {
-	[out = std::ref(std::cout << "Hello ")](){out.get() << "World\n";}();
+	asio::error_code ec;
+	asio::io_context context;
+
+	std::string address;
+	std::cout << "Enter the endpoint addres: ";
+	std::cin >> address;
+	asio::ip::tcp::endpoint ep(asio::ip::make_address(address), 80);
+	asio::ip::tcp::socket socket(context);
+	std::cout << "attempting to connect to endpoint...\n";
+	socket.connect(ep, ec);
+	if (!ec)
+	{
+		std::cout << "Success!\n";
+	}
+	else
+	{
+		std::cerr << "Failed to connect to endpoint:\n" << ec.message() << "\n";
+	}
+
+	return 0;
 }
